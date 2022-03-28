560771717a7f8d848934e32b49e6d6ce3362dfc4
bohdan skrypka <skripkabodya@gmail.com>
2022-03-28 15:01:51 +0300

    - add new solution folder with WinForms samples; add MultiThreadingDrawSample project to illustrate circles drawing

---
+++ Samples/MultiThreadingDrawSample/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace MultiThreadingDrawSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThread1 = new System.Windows.Forms.Button();
+            this.btnThread2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnThread1
+            // 
+            this.btnThread1.Location = new System.Drawing.Point(44, 27);
+            this.btnThread1.Name = "btnThread1";
+            this.btnThread1.Size = new System.Drawing.Size(94, 29);
+            this.btnThread1.TabIndex = 0;
+            this.btnThread1.Text = "Thread 1";
+            this.btnThread1.UseVisualStyleBackColor = true;
+            this.btnThread1.Click += new System.EventHandler(this.btnThread1_Click);
+            // 
+            // btnThread2
+            // 
+            this.btnThread2.Location = new System.Drawing.Point(168, 27);
+            this.btnThread2.Name = "btnThread2";
+            this.btnThread2.Size = new System.Drawing.Size(94, 29);
+            this.btnThread2.TabIndex = 1;
+            this.btnThread2.Text = "Thread 2";
+            this.btnThread2.UseVisualStyleBackColor = true;
+            this.btnThread2.Click += new System.EventHandler(this.btnThread2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThread2);
+            this.Controls.Add(this.btnThread1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThread1;
+        private System.Windows.Forms.Button btnThread2;
+    }
+}
+
---
+++ Samples/MultiThreadingDrawSample/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThreadingDrawSample
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThread1_Click(object sender, EventArgs e)
+        {
+            int pixelSize = 10;
+            Thread thread = new Thread(t =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = random.Next(0, this.Width);
+                    int height = random.Next(0, this.Height);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.Red, 1), new Rectangle(width, height, pixelSize, pixelSize));
+                    Thread.Sleep(100);
+                }
+            })
+            { IsBackground = true };
+
+            thread.Start();
+        }
+
+        private void btnThread2_Click(object sender, EventArgs e)
+        {
+            int pixelSize = 10;
+            Thread thread = new Thread(t =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = random.Next(0, this.Width);
+                    int height = random.Next(0, this.Height);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.Blue, 1), new Rectangle(width, height, pixelSize, pixelSize));
+                    Thread.Sleep(100);
+                }
+            })
+            { IsBackground = true };
+
+            thread.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            random = new Random();
+        }
+    }
+}
